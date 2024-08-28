b32be4cafa98eb96e83daa0ae1e40684b081e981
Angha <anghavarangaonkar1104@gmail.com>
2024-08-28 15:32:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// file ka first letter should be capital
+//jsx contains both html and react 
+import Button from "./button" 
+function App(){
+
+const firstname = 'angha'
+  return (
+    <div>
+      <Button/>
+     <h1>React</h1>
+     <p>hellooo world i m angha</p>
+     <p>{firstname}</p>
+    </div>
+
+  )
+}
+
+export default App
