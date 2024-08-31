7cc42a9cb83bec89beea9196905a7c5e1f7bdf09
Angha <anghavarangaonkar1104@gmail.com>
2024-08-31 15:28:44 +0530

    last commit

--- src/duplicateapp.jsx
+++ src/duplicateapp.jsx
@@ -122,8 +122,25 @@ function App(){
   )
  }
 
-export default App;
+// export default App;
 
+
+// day4:
+import Condition from "./components/Condition";
+import Render from "./components/Render";
+
+function App(){
+ 
+  return (
+    <div>
+      {/* <Render/> */}
+      <Condition/>
+    </div>
+
+  )
+ }
+
+export default App;
 
 
 
