c112a21f416dba3b666bf570aeed0f7fc96e5646
Angha <anghavarangaonkar1104@gmail.com>
2024-09-03 14:36:06 +0530

    day6

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Services from './pages/Services'
 import Navbar from './components/Navbar'
 import NotFound from './components/NotFound'
 import Ref from './components/Ref'
+// import Reducer from './pages/Reducer'
+const Reducer =lazy(()=>import('./pages/Reducer'))
+// lazy helps to load the data fast when we have data in the webpage 
+
+// import Usememo from './components/Usememo'
+
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      {/* <Usememo/> */}
+      <Suspense fallback={<div>loading</div>}>
       <Ref/>
       <Routes>
           
           <Route path='/' element={<Homepage/>}/>
           <Route path='/services' element={<Services/>}/>
 
+          <Route path='/reducer' element={<Reducer/>}/>
+
           <Route path='*' element={<NotFound/>}></Route>
       </Routes>
-      
+      </Suspense>
 
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar() {
     <div className='bg-violet-500 h-6 flex justify-center items-center gap-4' >
         <Link  to='/'>Home</Link>
         <Link to='/services'>Services</Link>
+        <Link to='/reducer'>UseReducer</Link>
       
     </div>
   )
